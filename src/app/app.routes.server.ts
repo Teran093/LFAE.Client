d979e94865d4f6ef0695aa9cf943c334b6cd3d58
Teran093 <isaacjosueteran@gmail.com>
2026-09-23 23:10:04 +0200

    feat: Create a home page and add it to the routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,14 +1,15 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  // {
-  //   path: '',
-  //   renderMode: RenderMode.Server,
-  // },
+  {
+    path: '',
+    renderMode: RenderMode.Server,
+  },
   {
     path: 'demo',
     renderMode: RenderMode.Client,
   },
+
   {
     path: '**',
     renderMode: RenderMode.Prerender,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./public/pages/home-page/home-page').then((m) => m.HomePage),
+    title: 'Home',
+  },
+
   // Demo page for testing components and features. This page is not part of the main
   // application and is only used for development purposes.
   {
