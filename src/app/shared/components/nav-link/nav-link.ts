b198fec3c4ecb87b1ab518a41904724976d4e90d
Teran093 <isaacjosueteran@gmail.com>
2026-09-25 00:36:11 +0200

    feat: Add overlay menu to the nav-link component for children components

--- src/app/shared/components/nav-link/nav-link.ts
+++ src/app/shared/components/nav-link/nav-link.ts
@@ -1,13 +1,16 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, contentChildren, input } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { OverlayDirective } from '../../directives/overlay.directive';
 
 @Component({
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, OverlayDirective],
   selector: 'app-nav-link',
   styleUrl: './nav-link.css',
   templateUrl: './nav-link.html',
 })
 export class NavLink {
+  protected children = contentChildren(NavLink);
+
   public navTitle = input<string>();
   public link = input<string>();
 
